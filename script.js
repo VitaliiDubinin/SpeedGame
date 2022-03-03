fd27003bd481e084a183a71056b8e4d070203910
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-03-03 14:07:50 +0200

    third

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const circles = document.querySelectorAll(".circle");
 const startButton = document.querySelector("#start");
 const endButton = document.querySelector("#stop");
+const overlay = document.querySelector("#overlay");
+const closeButton = document.querySelector("#close");
+let active = 0;
 
-const clickedCircle = () => {
-  console.log("circle was clicked");
+const clickedCircle = (i) => {
+  i += 1;
+  console.log("clicked circle was: ", i);
 };
 
 circles.forEach((circle, i) => {
   circle.addEventListener("click", () => clickedCircle(i));
 });
 
-const overlay = document.querySelector("#overlay");
-
-const closeButton = document.querySelector("#close");
+function getRndInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const startGame = () => {
   console.log("game started");
+  let nextActive = pickNew(active);
+  circles[nextActive].classList.toggle("active");
+  circles[active].classList.remove("active");
+  active = nextActive;
+  console.log("active circle:", active);
+  score += 1;
+  timer = setTimeout(startGame, 1000);
+
+  function pickNew(active) {
+    let nextActive = getRndInt(0, 3);
+
+    if (nextActive != active) {
+      return nextActive;
+    } else {
+      return pickNew(active);
+    }
+  }
 };
 
 const endGame = () => {
