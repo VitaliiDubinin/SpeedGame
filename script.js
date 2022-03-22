8f259c7b76298aea72326fc76295e932b3e46491
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-03-22 19:09:29 +0200

    add sound

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let pace = 1000;
 let rounds = 0;
 
 const clickedCircle = (i) => {
-  console.log("clicked circle was: ", i);
+  // console.log("clicked circle was: ", i);
   if (i !== active) {
     endGame();
   } else {
@@ -35,14 +35,15 @@ const startGame = () => {
   for (let i = 0; i < circles.length; i++) {
     circles[i].style.pointerEvents = "auto";
   }
+
   startButton.style.display = "none";
   endButton.style.display = "inline";
-  console.log("game started");
+  // console.log("game started");
   let nextActive = pickNew(active);
   circles[nextActive].classList.toggle("active");
   circles[active].classList.remove("active");
   active = nextActive;
-  console.log("active circle:", active);
+  // console.log("active circle:", active);
 
   timer = setTimeout(startGame, pace);
   pace = pace - 10;
@@ -63,7 +64,8 @@ const startGame = () => {
 };
 
 const endGame = () => {
-  console.log("game ended");
+  endGameMusic();
+  // console.log("game ended");
   clearTimeout(timer);
   overlay.style.visibility = "visible";
   resultText.textContent = `Your collect ${score} points`;
@@ -73,6 +75,36 @@ const reloadGame = () => {
   window.location.reload();
 };
 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = () => {
+    this.sound.play();
+  };
+  this.stop = () => {
+    this.sound.pause();
+  };
+}
+
+const startGameMusic = () => {
+  // startSound = new sound("./sounds/tetris-gameboy-02.mp3");
+  startSound = new sound("./02 Story.flac");
+  startSound.play();
+};
+
+const endGameMusic = () => {
+  endSound = new sound("./01. Sega Intro.flac");
+  startSound.stop();
+  endSound.play();
+};
+
 startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startGameMusic);
+endButton.addEventListener("click", endGameMusic);
 endButton.addEventListener("click", endGame);
+closeButton.addEventListener("click", endGameMusic);
 closeButton.addEventListener("click", reloadGame);
